aea2c8c82c902904db00a8bf1eb1052279763ae9
MingYuan200 <yuan40133@gmail.com>
2024-12-25 00:41:41 +0800

    1225--+

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
+// 動態設置 basename
+const basename = process.env.NODE_ENV === 'production' ? '/my-craftopiaPages' : '/';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/my-craftopiaPages">
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter >
   </React.StrictMode>,
